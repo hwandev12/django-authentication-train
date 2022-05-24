414813fb571a241ad5e480b4f8c43f912547e27a
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-24 21:52:29 +0500

    add auth user model and logi redirect url

--- core/settings.py
+++ core/settings.py
@@ -84,6 +84,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'authentication.CustomUserModel'
+LOGIN_REDIRECT_URL = '/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
