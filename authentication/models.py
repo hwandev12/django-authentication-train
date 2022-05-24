6c047d6fcba0cfd4f4d72ef45955413b6cfe5361
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-24 21:52:39 +0500

    create abstract user

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# create custom user model
+class CustomUserModel(AbstractUser):
+    pass
