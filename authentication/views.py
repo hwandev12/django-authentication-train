de2df31d5dd9d838f19cd36d0d1062ac0e88665c
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-24 23:05:58 +0500

    create sign up view

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.views.generic.edit import CreateView
+from .forms import CustomUserModelCreate
 
 
 # create home template
 def home(request):
     return render(request, 'pages/home.html')
+
+
+# create signup view
+class SignUpView(CreateView):
+    form_class = CustomUserModelCreate
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return reverse('login')
